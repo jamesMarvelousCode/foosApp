f0f295290db5ef829e01623df7bd0fa52b066b8a
jimmyjames <jamesd8568@gmail.com>
2018-03-08 10:32:48 -0600

    Updated app flow with a more dynamic form as opposed to several forms

--- fooApp_v3/fooApp_v3/Program.cs
+++ fooApp_v3/fooApp_v3/Program.cs
@@ -16,7 +16,7 @@ namespace fooApp_v3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new MainForm());
         }
     }
 }
